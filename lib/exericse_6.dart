1ba06372b78000063d1a19e704b4e41db81bb22e
Sagar Gurung <gurungy689@gmail.com>
2022-12-12 14:31:50 +0545

    Exericse 5 added

---
+++ lib/exericse_6.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Exercise6 extends StatefulWidget {
+  const Exercise6({super.key});
+
+  @override
+  State<Exercise6> createState() => Exercise6State();
+}
+
+class Exercise6State extends State<Exercise6> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
